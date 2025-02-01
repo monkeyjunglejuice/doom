ef0dd4ed09f3a243b3784d7b0cc593b3812ef53b
Dan Dee <monkeyjunglejuice@pm.me>
2025-02-01 17:35:43 +0800

    Switch to Modus themes. Quite legible in sunlight

--- config.el
+++ config.el
@@ -33,16 +33,27 @@
 ;;  ____________________________________________________________________________
 ;;; THEMES
 
-;; Theme settings
-(after! modus-themes
-  (setq! modus-themes-bold-constructs t))
+;; Modus theme settings
+(use-package! modus-themes
+  :config
+  (setq modus-themes-bold-constructs t
+        modus-themes-italic-constructs nil
+        modus-themes-mixed-fonts t)
+  (setq modus-themes-common-palette-overrides
+        '((border-mode-line-active unspecified)
+          (border-mode-line-inactive unspecified)))
+  (setq modus-operandi-tinted-palette-overrides
+        modus-themes-preset-overrides-warmer)
+  (setq modus-vivendi-tinted-palette-overrides
+        modus-themes-preset-overrides-warmer))
 
-(after! ef-themes
-  (setq! ef-themes-mixed-fonts t))
+;; Do not extend `region' background past the end of the line.
+(custom-set-faces
+ '(region ((t :extend nil))))
 
 ;;; - Set light/dark theme:
-(setq! my-theme-light 'ef-light)
-(setq! my-theme-dark 'ef-dark)
+(setq my-theme-light 'modus-operandi-tinted)
+(setq my-theme-dark 'modus-vivendi-tinted)
 
 ;; Switch between dark/light theme based on the system appearance
 ;; <https://github.com/d12frosted/homebrew-emacs-plus?tab=readme-ov-file#system-appearance-change>
