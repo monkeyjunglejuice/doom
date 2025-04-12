9fb59161a92221f0d24c16515db6a54a40a8c436
Dan Dee <monkeyjunglejuice@pm.me>
2025-04-12 12:43:41 +0800

    Elixir: formatting and commentary

--- modules/lang/elixir-contrib/+iex.el
+++ modules/lang/elixir-contrib/+iex.el
@@ -3,11 +3,13 @@
 (use-package! inf-elixir
   :autoload
   inf-elixir--find-project-root
+
   :init
   (set-repl-handler! 'elixir-mode #'inf-elixir-run)
   (set-eval-handler! 'elixir-mode #'inf-elixir-send-region)
+
   (defun inf-elixir-run ()
-    "Start IEx in the project context when a Mix project is detected.
+    "Starts IEx in the project context when a Mix project is detected.
 The commands are `inf-elixir-project-command' (defaults to \"iex -S mix\") and
 `inf-elixir-base-command' (defaults to \"iex\")."
     (interactive)
@@ -17,10 +19,19 @@ The commands are `inf-elixir-project-command' (defaults to \"iex -S mix\") and
                (inf-elixir-project))
       (progn (message "inf-elixir: IEx running standalone...")
              (inf-elixir))))
+
   (defalias 'run-elixir #'inf-elixir-run "Alias for `inf-elixir-run'")
+
   :config
   (set-popup-rule! "^\\*Inf-Elixir.*\\*" :size 0.35 :quit nil :ttl nil)
   (setq! inf-elixir-switch-to-repl-on-send nil)
+
+  (defun inf-elixir-switch-to-repl ()
+    "Switch to IEx buffer."
+    (interactive)
+    (when-let* ((buf (inf-elixir--determine-repl-buf)))
+      (pop-to-buffer buf)))
+
   (defun inf-elixir-recompile ()
     "Send `IEx.Helpers.recompile/1' to recompile the current Mix project.
 This function simply recompiles Elixir modules, without reloading configuration
@@ -31,15 +42,18 @@ without going through the proper code change callback."
     (inf-elixir--send (format "recompile()"))
     (if inf-elixir-switch-to-repl-on-send
         (goto-char (point-max))))
+
   (defun inf-elixir-observer ()
     "Start the Erlang Observer in IEx."
     (interactive)
     (inf-elixir--send (format ":observer.start()")))
+
   (map! :localleader
         (:map elixir-mode-map
          :n "'" #'inf-elixir-run
+         :n "SPC" #'inf-elixir-switch-to-repl
          :n "r" #'inf-elixir-reload-module
-         :n "c" #'inf-elixir-recompile
+         :n "R" #'inf-elixir-recompile
          :n "o" #'inf-elixir-observer
          (:prefix ("e" . "eval")
           :n "b" #'inf-elixir-send-buffer
@@ -47,8 +61,9 @@ without going through the proper code change callback."
           :v "r" #'inf-elixir-send-region))
         (:map inf-elixir-mode-map
          :n "r" #'inf-elixir-reload-module
-         :n "c" #'inf-elixir-recompile
+         :n "R" #'inf-elixir-recompile
          :n "o" #'inf-elixir-observer))
+
   :hook
   (elixir-mode . inf-elixir-minor-mode)
   (inf-elixir-mode . rainbow-delimiters-mode))
